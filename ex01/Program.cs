4fa8a49def5a0709db82c28b2314bcfe687238e2
OlegLegezo <oleglegezo@yandex.ru>
2022-06-03 22:37:59 +0300

    переворот массива

---
+++ ex01/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 39: Напишите программу, которая перевернёт
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+Console.Write("Введите элементы массива через пробел: ");
+int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();//пачка конвертаций из семинара, надо разобраться
+
+Console.WriteLine(String.Join(" ", array));
+
+int size = array.Length;
+int index1 = 0;
+int index2 = size - 1;
+
+//алгоритм переворачивания масива
+while (index1 < index2)
+{
+    int temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+    index1++;
+    index2--;
+}
+//алгоритм переворачивания масива
+
+Console.WriteLine(String.Join(" ", array));
