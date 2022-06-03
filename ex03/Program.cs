82b9a3def503bb6c62522104d5180d820afc2d28
OlegLegezo <oleglegezo@yandex.ru>
2022-06-03 23:40:01 +0300

    перевод из 10 в бинарную

---
+++ ex03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("введите десятичное положительное число: ");
+int a = int.Parse(Console.ReadLine());
+
+//метод перевода
+void BinNum(int num)
+{
+    string binnum = "";
+    int decnum = num;
+    while (num > 0)
+    {
+        string text = (num % 2).ToString();
+        num = num / 2;
+        binnum = text + binnum;
+    }
+    Console.WriteLine($"Число {decnum} в двоичном формате равно: {binnum}");
+}
+//метод перевода
+
+BinNum(a);
