e5cc97ee12dff9839c945254b329659aa07fe46d
OlegLegezo <oleglegezo@yandex.ru>
2022-06-04 00:15:09 +0300

    перевод из 10 в бинарную

--- ex03/Program.cs
+++ ex03/Program.cs
@@ -7,7 +7,7 @@
 Console.Write("введите десятичное положительное число: ");
 int a = int.Parse(Console.ReadLine());
 
-//метод перевода
+//метод перевода 1
 void BinNum(int num)
 {
     string binnum = "";
@@ -20,6 +20,35 @@ void BinNum(int num)
     }
     Console.WriteLine($"Число {decnum} в двоичном формате равно: {binnum}");
 }
-//метод перевода
+//метод перевода 1
 
-BinNum(a);
+//BinNum(a);
+
+
+//способ перевода 2
+Console.Write("введите основание системы счисления: ");
+int baseNum = int.Parse(Console.ReadLine());
+// int d10 = 1;
+// long result = 0;
+// while (a != 0)
+// {
+//     result = result + a % baseNum * d10;
+//     a /= baseNum; //a=a/2 тоже самое  a /= 2
+//     d10 = d10 * 10;
+// }
+// Console.WriteLine(result);
+//способ перевода 2
+
+
+
+//способ перевода 3
+void DecToOther (int num, int baseN)
+{
+    if (num==0)
+    return;
+    DecToOther (num/baseN, baseN);
+    Console.Write(num%baseN);
+}
+
+DecToOther(a,baseNum);
+//способ перевода 3
